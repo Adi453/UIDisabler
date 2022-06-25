d22e96d7f28a5fa61a943079a9b7b85cde61ebec
Adi <Adi453@users.noreply.github.com>
2022-06-25 03:47:33 +0300

    Code cleanup

--- MelonLoaderMod.cs
+++ MelonLoaderMod.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using ModThatIsNotMod;
 using ModThatIsNotMod.BoneMenu;
 using Object = UnityEngine.Object;
-using Colour = UnityEngine.Color;
+using Colour = UnityEngine.Color; //god save the queen
 using HarmonyLib;
 using StressLevelZero.Props.Weapons;
 using System.Reflection;
@@ -41,32 +41,21 @@ namespace UIDisabler
                 if (meth.Name.Contains("HandHover"))
                     HarmonyInstance.Patch(meth, prefix: typeof(UIDisabler).GetMethod("Prefix").ToNewHarmonyMethod());
             }
-                
             MenuCategory cat = MenuManager.CreateCategory("UI Disabler", Colour.white);
-            cat.CreateBoolElement("Disable Force Pull / Grab Indicator", Colour.white, forcePull, delegate (bool value)
-            {
-                forcePull = value;
-            });
-            
+            cat.CreateBoolElement("Disable Force Pull / Grab Indicator", Colour.white, forcePull, delegate (bool value) { forcePull = value; });
             cat.CreateBoolElement("Disable Ammo UI", Colour.white, ammo, delegate (bool value)
             {
                 ammo = value;
                 hud.SetActive(!ammo);
             });
-            cat.CreateBoolElement("Disable Gun Reload Helper Thing", Colour.white, gun, delegate (bool value)
-            {
-                gun = value;
-            });
+            cat.CreateBoolElement("Disable Gun Reload Helper Thing", Colour.white, gun, delegate (bool value) { gun = value; });
         }
-        public static void Prefix(InteractableIcon __instance) => __instance.IconSize = forcePull? 0 : 0.015f;
+        public static void Prefix(InteractableIcon __instance) => __instance.IconSize = forcePull ? 0 : 0.015f;
 
-        
         [HarmonyPatch(typeof(Gun), "CoBlinkHighlight")]
         public static class GunPatch
         {
             public static bool Prefix(Gun __instance) => !gun;
         }
-
-        
     }
 }
